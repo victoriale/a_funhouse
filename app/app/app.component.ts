9cb6f72d800970eaaf75104d828efceb553e89a7
ricksellers <rick@ricksellers.design>
2016-02-22 14:40:12 -0600

    Moved app folder out of components into main folder

--- app/app/app.component.ts
+++ app/app/app.component.ts
@@ -1,13 +1,13 @@
 import {Component} from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
-import {ListOfListModule} from '../../modules/listoflist/listoflist.module';
-import {ProfilePage} from "../../webpages/profile-page/profile.page";
-import {HomePage} from "../../webpages/home-page/home.page";
+import {ListOfListModule} from '../modules/listoflist/listoflist.module';
+import {ProfilePage} from "../webpages/profile-page/profile.page";
+import {HomePage} from "../webpages/home-page/home.page";
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app/components/app/app.component.html',
+    templateUrl: './app/app/app.component.html',
     styleUrls: ['./app/global/stylesheets/master.css'],
     directives: [ProfilePage, HomePage, ROUTER_DIRECTIVES],
     providers: [ROUTER_PROVIDERS]
--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,7 @@
 ///<reference path="../node_modules/angular2/typings/es6-promise/es6-promise.d.ts"/>
 
 import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './components/app/app.component'
+import {AppComponent} from './app/app.component'
 
 // Needed for http map on observables
 import 'rxjs/add/operator/map';
