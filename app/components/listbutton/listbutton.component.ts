7f462491cfb5417d62d8a5d59e0a5c055d985421
ricksellers <rick@ricksellers.design>
2016-02-22 14:25:17 -0600

    Removed duplicate list button component

--- app/components/listbutton/listbutton.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'listbutton-component',
-    templateUrl: './app/components/listbutton/listbutton.component.html',
-    styleUrls: ['./app/global/stylesheets/master.css'],
-})
-export class ListButtonComponent { }
