6899346cff9959eeb305307282e37f4739f1d98a
lutzklai <lutzklai@gmail.com>
2016-02-29 11:03:12 -0600

    first rev of crime module added

---
+++ app/modules/crime/crime.module.ts
@@ -0,0 +1,71 @@
+import {Component, OnInit} from 'angular2/core';
+
+import {moduleHeader} from '../../components/module-header/module-header';
+
+@Component({
+    selector: 'crime-module',
+    templateUrl: './app/modules/crime/crime.module.html',
+    styleUrls: ['./app/global/stylesheets/master.css'],
+    directives: [moduleHeader],
+    providers: []
+})
+
+export class CrimeModule implements OnInit{
+    module_title: string;
+    stats = [
+        {
+            title: 'Thefts',
+            color: '#44b244',
+            pct: '75%'
+        },
+        {
+            title: 'Assault',
+            color: '#1C31CE',
+            pct: '10%'
+        },
+        {
+            title: 'Homicides',
+            color: '#FFC600',
+            pct: '10%'
+        },
+        {
+            title: 'Other',
+            color: '#898989',
+            pct: '5%'
+        }
+    ];
+    crime_list = [
+        {
+            type: 'Shooting',
+            description: 'Shooting/Stabbing. CPD o/s with person stabbed in head during quarrel. Victim is in good condition...',
+            date: '02/23/2016',
+            img_id: 'shooting',
+            img_src: './app/public/icons/Icon_Shooting.png'
+        },
+        {
+            type: 'Arrest',
+            description: 'INTERFERENCE WITH PUBLIC OFFICER. OBSTRUCTING JUSTICE. Location description: STREET...',
+            date: '02/23/2016',
+            img_id: 'arrest',
+            img_src: './app/public/icons/Icon_Arrest.png'
+        },
+        {
+            type: 'Vandalism',
+            description: 'CRIMINAL DAMAGE TO VEHICLE. Location description: CHA PARKING LOT/GROUNDS...',
+            date: '02/23/2016',
+            img_id: 'vandalism',
+            img_src: './app/public/icons/Icon_Vandalism.png'
+        },
+        {
+            type: 'Theft',
+            description: 'THEFT. $500 AND UNDER. Location description. GAS STATION...',
+            date: '02/23/2016',
+            img_id: 'theft',
+            img_src: './app/public/icons/Icon_Theft.png'
+        }
+    ]
+
+    ngOnInit(){
+        this.module_title = 'Crime Activity In and Around [Listing Name]';
+    }
+}
--- app/webpages/profile-page/profile.page.ts
+++ app/webpages/profile-page/profile.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from 'angular2/core';
 
 import {ListOfListModule} from '../../modules/listoflist/listoflist.module';
+import {CrimeModule} from '../../modules/crime/crime.module';
 import {HeaderComponent} from "../../components/header/header.component";
 import {FooterComponent} from "../../components/footer/footer.component";
 
@@ -8,7 +9,7 @@ import {FooterComponent} from "../../components/footer/footer.component";
     selector: 'profile-page',
     templateUrl: './app/webpages/profile-page/profile.page.html',
     styleUrls: ['./app/global/stylesheets/master.css'],
-    directives: [ListOfListModule, HeaderComponent, FooterComponent],
+    directives: [ListOfListModule, CrimeModule, HeaderComponent, FooterComponent],
     providers: [],
 })
 
