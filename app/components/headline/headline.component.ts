9e5325cafdf0eebe70b218e76fec073237bccb4f
lutzklai <lutzklai@gmail.com>
2016-03-01 11:39:59 -0600

    Added headline component to location page and listing page

--- app/components/headline/headline.component.ts
+++ app/components/headline/headline.component.ts
@@ -1,15 +1,14 @@
 /**
  * Created by Victoria on 2/23/2016.
  */
-import {Component} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
 
 @Component({
     selector: 'headline-component',
     templateUrl: './app/components/headline/headline.component.html',
     styleUrls: ['./app/global/stylesheets/master.css'],
+    inputs: ['title']
 })
 export class HeadlineComponent{
-    heading= 'About';
-    name = '[Listing Name]';
-    icon = 'fa fa-map-marker';
+    public title: string;
 }
