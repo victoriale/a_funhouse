f38ee187939f4e946e730a0c5e99eb0f43a30bc0
lynchtroop5 <christopher.lynch@sntmedia.com>
2016-03-04 16:06:58 -0600

    Even more changes to the magazine map.

--- app/modules/magazine/mag-map/mag-map.module.ts
+++ app/modules/magazine/mag-map/mag-map.module.ts
@@ -10,13 +10,6 @@ import {MagMapData} from "../../../global/global-interface";
     styleUrls: ['./app/global/stylesheets/master.css'],
     directives: [],
     providers: [MagazineMap],
-    styles: [`
-    .sebm-google-map-container {
-       height: 24.896vw;
-     }
-     .test1 {
-     width: 100px;
-  `],
 })
 
 
@@ -37,7 +30,6 @@ export class MagMapModule implements OnInit {
     ngOnInit() {
         this._magazineMapService.getMagazineMap().then(data => {
             this.data = data;
-
             var myLatlng = new google.maps.LatLng(this.data[1]);
             var mapOptions = {
                 zoom: 12,
@@ -45,15 +37,39 @@ export class MagMapModule implements OnInit {
             };
             var map = new google.maps.Map(document.getElementById("map"), mapOptions);
             for (var i = 0; i < data.length; i++) {
-                var home = this.data[i];
-                console.log(home);
+                var home = new google.maps.LatLng(this.data[i]);
+
+                if ( this.data[i].list_price >= 1000000 ) {
+                    var priceString = "$" + (Math.round(this.data[i].list_price/100000)/10) + "M";
+                } else {
+                    var priceString = "$" + (Math.round(this.data[i].list_price/1000)) + "K";
+                }
+                if ( i == 0 ) {
+                    var winString = '<div class="map_item focus" id="' + i + '">' + priceString + '</div>';
+                } else {
+                    var winString = '<div class="map_item" id="' + i + '">' + priceString + '</div>';
+                }
+                var infoWindow = new google.maps.InfoWindow({
+                    content: winString,
+                    position: home
+                });
+                infoWindow.open(map);
+                //google.maps.event.addListener(infoWindow, 'domready', function(){
+                //    $('.gm-style-iw').prev().addClass('hide'); $('.gm-style-iw').next().addClass('hide');
+                //    $('.gm-style-iw').parent().addClass('zSize');
+                //});
                 var marker = new google.maps.Marker({
-                    position: {lat: home[1], lng: home[2]},
+                    position: home,
                     map: map,
+                    //title: priceString,
+                    //icon: image,
                 });
             }
 // To add the marker to the map, call setMap();
             marker.setMap(map);
         });
-    }
+    }//google.maps.event.addListener(infoWindow, 'domready', function(){
+//    document.getElementsByClassName('.gm-style-iw').prev().classList('hide'); document.getElementsByClassName('.gm-style-iw').next().classList('hide');
+//    document.getElementsByClassName('.gm-style-iw').parent().classList('zSize');
+//});
 }
