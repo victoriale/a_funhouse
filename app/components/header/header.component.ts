4a618c336e2d98cd7ee077cf461d014830011dba
ricksellers <rick@ricksellers.design>
2016-03-11 10:58:56 -0600

    Directories dropdown with placeholder list data

--- app/components/header/header.component.ts
+++ app/components/header/header.component.ts
@@ -13,5 +13,36 @@ export class HeaderComponent{
 
     public isHomePage: boolean = false;
     public isMyHouseKit: boolean = false;
+    visible: boolean;
+
+    constructor() {
+       this.visible = false;
+    }
+
+    directoryList = [
+        "Most expensive 2 bedroom homes",
+        "Most expensive 3 bedroom homes",
+        "Most expensive condos",
+        "Least expensive homes with waterfront",
+        "Largest homes",
+        "Most expensive homes",
+        "Cheapest Homes",
+        "Least expensive homes with a swimming pool",
+        "Least expensive brick houses",
+        "Homes less than 5 years old",
+        "Listings with more than 5 photos",
+        "Listings with virtual tours",
+        "Homes with sprinkler system and deck",
+        "New traditional homes",
+        "Homes with vaulted ceiling and security",
+        "Listings in wealthiest ZIP code in area",
+        "Homes at least 5 years old",
+        "Listings with more than 10 photos",
+        "Listings with long descriptions",
+    ];
+
+    directoryToggle() {
+        this.visible = !this.visible;
+    }
 
 }
