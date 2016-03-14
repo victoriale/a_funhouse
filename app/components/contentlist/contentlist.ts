63af293befa23cca4bc3fcc4a4663aa88aa82027
lutzklai <lutzklai@gmail.com>
2016-03-14 13:50:23 -0500

    added modules to location page

--- app/components/contentlist/contentlist.ts
+++ app/components/contentlist/contentlist.ts
@@ -1,27 +1,21 @@
 import {Component, Input, OnInit} from 'angular2/core';
 import {List} from '../../global/global-interface';
-import {ListOfListService} from '../../global/global-service';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
     selector: 'contentlist',
     templateUrl: './app/components/contentlist/contentlist.html',
     styleUrls: ['./app/global/stylesheets/master.css'],
-    directives: [ROUTER_DIRECTIVES],
-    providers: [ListOfListService],
+    directives: [],
+    providers: [],
 })
 
 export class contentList implements OnInit{
-    @Input() lol;
-
-    constructor(
-        private _listService: ListOfListService
-    ) { }
+    @Input() data;
 
     dummyData(){
         //if no data then dummy data is returned
-        if(typeof this.lol == 'undefined'){
-            this.lol =
+        if(typeof this.data == 'undefined'){
+            this.data =
             {
                 'title': '[List Name Here] Lorem ipsum dolor sit amet[List Name Here] Lorem ipsum dolor sit amet[List Name Here] Lorem ipsum dolor sit amet',
                 'bigImage': './app/public/img_bckgnd.png',
