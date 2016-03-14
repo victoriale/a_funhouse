9712b4a74a3fe7b8ef570ccdbe0f6e751ce76673
ricksellers <rick@ricksellers.design>
2016-03-14 12:39:50 -0500

    List carousel component, list of lists page, package.json update to remove ng2-bootstrap

---
+++ app/components/list-carousel/list-carousel.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from 'angular2/core';
+import {Image180} from "../images/image-180.component";
+import {CircleButton} from "../buttons/circle/circle.button";
+
+@Component({
+    selector: 'list-carousel-component',
+    templateUrl: './app/components/list-carousel/list-carousel.component.html',
+    styleUrls: ['./app/global/stylesheets/master.css'],
+    directives: [Image180, CircleButton],
+    providers: [],
+})
+
+export class ListCarouselComponent implements OnInit {
+    public icon: string;
+    public title: string;
+    public description: string;
+    public main_hasSubImg: boolean = false;
+
+    setStaticData(){
+        this.icon = 'fa fa-home';
+        this.title = '[List Name]';
+        this.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam purus justo, semper porta blandit non, auctor ac nulla. Ut vitae quam at augue sodales commodo et eget diam. Curabitur venenatis a sapien id pharetra.';
+    }
+
+    ngOnInit(){
+        this.setStaticData();
+    }
+}
--- app/webpages/list-of-lists-page/list-of-lists.page.ts
+++ app/webpages/list-of-lists-page/list-of-lists.page.ts
@@ -1,16 +1,21 @@
 import {Component} from 'angular2/core';
 import {BackTabComponent} from "../../components/backtab/backtab.component";
 import {TitleComponent} from "../../components/title/title.component";
+import {ListCarouselComponent} from "../../components/list-carousel/list-carousel.component";
 
 @Component({
     selector: 'list-of-lists-page',
     templateUrl: './app/webpages/list-of-lists-page/list-of-lists.page.html',
     styleUrls: ['./app/global/stylesheets/master.css'],
-    directives: [BackTabComponent, TitleComponent],
+    directives: [BackTabComponent, TitleComponent, ListCarouselComponent],
     providers: [],
 })
 
-export class ListOfListsPage {
+export class ListOfListsPage{
+    public icon: string;
+    public title: string;
+    public description: string;
+    public main_hasSubImg: boolean = true;
 
     constructor() {
         // Scroll page to top to fix routerLink bug
