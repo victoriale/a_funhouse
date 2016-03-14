7e4bcae67234c05fc6d5a399ee850b96685704e8
ricksellers <rick@ricksellers.design>
2016-03-14 14:54:21 -0500

    Created hero-list component and added it to listoflists page

---
+++ app/components/hero/hero-list/hero-list.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+import {HeroSearchComponent} from "../hero-search/hero-search.component";
+
+@Component({
+    selector: 'hero-list-component',
+    templateUrl: './app/components/hero/hero-list/hero-list.component.html',
+    styleUrls: ['./app/global/stylesheets/master.css'],
+    directives: [HeroSearchComponent],
+    providers: [],
+})
+
+export class HeroListComponent{
+    heroListTitle: string = "Discover Your Next Home";
+    heroListSubTitle: string = "Get all the details you need to plan your future.";
+}
--- app/components/list-carousel/list-carousel.component.ts
+++ app/components/list-carousel/list-carousel.component.ts
@@ -1,15 +1,12 @@
 import {Component, OnInit} from 'angular2/core';
 import {Image180} from "../images/image-180.component";
-import {List} from '../../global/global-interface';
-import {ListOfListService, BatchOne} from '../../global/global-service';
-import {contentList} from "../../components/contentlist/contentlist";
 
 @Component({
     selector: 'list-carousel-component',
     templateUrl: './app/components/list-carousel/list-carousel.component.html',
     styleUrls: ['./app/global/stylesheets/master.css'],
-    directives: [Image180, contentList],
-    providers: [ListOfListService, BatchOne],
+    directives: [Image180],
+    providers: [],
 })
 
 export class ListCarouselComponent implements OnInit {
@@ -17,21 +14,6 @@ export class ListCarouselComponent implements OnInit {
     public title: string;
     public description: string;
     public main_hasSubImg: boolean = false;
-
-    lists : List[];
-    batch1: List[];
-
-    constructor(
-        private _listService: ListOfListService,
-        private _batchone: BatchOne
-    ) { }
-
-    getListOfList() {
-        this._listService.getListOfList().then(lists => this.lists = lists);
-    }
-    getBatchOne(){
-        this._batchone.getBatchOne().then(batch1 => this.batch1 = batch1);
-    }
 
     setStaticData(){
         this.icon = 'fa fa-home';
@@ -41,7 +23,5 @@ export class ListCarouselComponent implements OnInit {
 
     ngOnInit(){
         this.setStaticData();
-        this.getListOfList();
-        this.getBatchOne();
     }
 }
