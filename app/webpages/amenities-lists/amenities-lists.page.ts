41a15bcd8fab37c00528f5565266bc1d2c4b58f3
victoriale <le.victoriavan@gmail.com>
2016-03-14 18:23:09 -0500

    add amenities list view page

---
+++ app/webpages/amenities-lists/amenities-lists.page.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Victoria on 3/14/2016.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'Amenities-list-page',
+    templateUrl: './app/webpages/amenities-lists/amenities-lists.page.html',
+    styleUrls: ['./app/global/stylesheets/master.css'],
+    directives: [],
+})
+
+export class AmenitiesListPage {}
