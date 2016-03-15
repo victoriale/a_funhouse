0e16a498b407a6daf0654836ffef460dcb4ddccd
lutzklai <lutzklai@gmail.com>
2016-03-15 13:19:41 -0500

    cleaned up feature list code

--- app/modules/featured_lists/featured_lists.module.ts
+++ app/modules/featured_lists/featured_lists.module.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Input, OnChanges} from 'angular2/core';
-import {Router} from 'angular2/router';
+import {Router, RouteParams} from 'angular2/router';
 
 import {moduleHeader} from '../../components/module-header/module-header';
 import {TilesComponent} from '../../components/tiles/tiles.component';
@@ -16,16 +16,38 @@ import {GlobalFunctions} from '../../global/global-functions';
 })
 
 export class FeaturedListsModule implements OnInit{
-    profileType: string;
-    moduleTitle: string;
-    tileData: Object;
-    listData: Object;
-    index: number;
+    public profileType: string;
+    public moduleTitle: string;
+    public tileData: Object;
+    public listData: Object;
+    public index: number;
     @Input() featuredListData: FeaturedListInterface;
 
-    constructor(private router: Router, private globalFunctions: GlobalFunctions){
+    constructor(private router: Router, private _params: RouteParams, private globalFunctions: GlobalFunctions){
         //Determine what page the profile header module is on
         this.profileType = this.router.hostComponent.name;
+    }
+
+    //Build Module Title
+    setModuleTitle(){
+        if(this.profileType === 'LocationPage'){
+            //Location Featured List Module
+
+            var paramLocation: string = this._params.get('loc');
+            var paramCity: string = this.globalFunctions.toTitleCase(paramLocation.split('_')[0]);
+            var paramState: string = paramLocation.split('_')[1];
+
+            this.moduleTitle = 'Featured Lists for ' + paramCity + ', ' + paramState;
+        }else if(this.profileType === 'ProfilePage'){
+            //Listing Crime Module
+            var paramAddress = this._params.get('address').split('-');
+            var paramState = paramAddress[paramAddress.length -1];
+            var paramCity = paramAddress [paramAddress.length - 2];
+            var tempArr = paramAddress.splice(-paramAddress.length, paramAddress.length - 2);
+            var address = tempArr.join(' ');
+
+            this.moduleTitle = 'Featured List for ' + address + ' ' + paramCity + ', ' + paramState;
+        }
     }
 
     left(){
@@ -68,7 +90,8 @@ export class FeaturedListsModule implements OnInit{
         //Initialize index value
         this.index = 0;
 
-        this.moduleTitle = 'Featured Lists for [Listing Name]';
+        this.setModuleTitle();
+        //Set static data - Will remove when routes further defined
         this.tileData = {
             button_txt: 'Open Page',
             url1: '',
@@ -83,15 +106,6 @@ export class FeaturedListsModule implements OnInit{
             icon3: 'fa-th-large',
             title3: 'Similar Top 100 Lists',
             desc3: ''
-        }
-        this.listData = {
-            header: 'Trending Real Estate',
-            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do.',
-            hding1: '[Listing Address]',
-            hding2: '[Listing Name] [Zip Code] - [Neighborhood]',
-            detail1: 'Bedrooms: 3 | Bathrooms: 2',
-            detail2: 'Asking Price: ',
-            detail3: '$[###,###]'
         }
 
     }
