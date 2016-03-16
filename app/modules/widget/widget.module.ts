85924e0b396e9871edd16a8ded810214b7c06307
ricksellers <rick@ricksellers.design>
2016-03-16 16:54:09 -0500

    Added module for widget, fixed info-list errors

---
+++ app/modules/widget/widget.module.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'widget',
+    templateUrl: './app/components/widget/widget.component.html',
+    styleUrls: ['./app/global/stylesheets/master.css'],
+    directives: [],
+    providers: [],
+})
+
+export class WidgetComponent {
+}
