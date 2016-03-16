3a6b19e60698b85e5d6e6e4d0c4d55aa17b9c775
lutzklai <lutzklai@gmail.com>
2016-03-16 16:13:08 -0500

    added dropdown to hero search. Fixed vlicker issue with search dropdown

--- app/global/search-service.ts
+++ app/global/search-service.ts
@@ -1,7 +1,5 @@
 import {Injectable} from 'angular2/core';
 import {Http, Headers} from 'angular2/http';
-import {Observable} from "rxjs/Observable";
-
 
 @Injectable()
 
@@ -16,11 +14,6 @@ export class SearchService{
         //Configure HTTP Headers
         var headers = new Headers();
         //headers.append('X-SNT-TOKEN', 'BApA7KEfj');
-
-        //If input parameter is blank, send back empty array observable. This is used by the search results component to delete search results
-        if(input === ''){
-            return Observable([]);
-        }
 
         input = encodeURI(input);
 
@@ -48,39 +41,54 @@ export class SearchService{
     //API to transform data to link
     dataToLink(data){
         var count = 0;
+        var maxCount = 10;
         var searchArray = [];
 
-        data.address.forEach(function(item, index){
-            searchArray.push({
-                title: item.address_key.replace(/-/g, ' '),
-                page: 'Profile-page',
-                params: {
-                    address: item.address_key
-                }
+        //If addresses are defined in api return iterate through addresses
+        if(typeof data.address !== 'undefined') {
+            data.address.forEach(function (item, index) {
+                searchArray.push({
+                    title: item.address_key.replace(/-/g, ' '),
+                    page: 'Profile-page',
+                    params: {
+                        address: item.address_key
+                    }
+                });
+                //Increment counter to ensure only 10 results are displayed
+                count++;
             });
-            count++;
-        });
+        }
 
-        data.city.forEach(function(item, index){
-            searchArray.push({
-                title: item.city + ', ' + item.state_or_province,
-                page: 'Location-page',
-                params: {
-                    loc: item.city + '_' + item.state_or_province
-                }
-            });
-            count++;
-        });
-
-        if(typeof data.zipcode !== 'undefined' && count < 10){
-            data.zipcode.forEach(function(item, index){
+        //If location cities are defined in api return iterate through cities
+        if(typeof data.location_city !== 'undefined') {
+            data.location_city.forEach(function (item, index) {
                 searchArray.push({
                     title: item.city + ', ' + item.state_or_province,
                     page: 'Location-page',
                     params: {
                         loc: item.city + '_' + item.state_or_province
                     }
-                })
+                });
+                //Increment count to ensure only 10 results are displayed
+                count++;
+            });
+        }
+
+        //If zipcodes are defined in api return iterate through zipcodes
+        if(typeof data.zipcode !== 'undefined' && count < 10){
+            data.zipcode.forEach(function(item, index){
+                //If count is 10 skip over remaining zipcodes
+                if(count < maxCount) {
+                    searchArray.push({
+                        title: item.city + ', ' + item.state_or_province,
+                        page: 'Location-page',
+                        params: {
+                            loc: item.city + '_' + item.state_or_province
+                        }
+                    });
+                    //Increment count to ensure only 10 results are displayed
+                    count++;
+                }
             })
         }
 
