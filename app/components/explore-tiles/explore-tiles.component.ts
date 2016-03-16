2223a9246b4e91e17458ce384216a56fa0468804
ricksellers <rick@ricksellers.design>
2016-03-16 13:33:14 -0500

    Home page dropdown to change location for testing geo location call

--- app/components/explore-tiles/explore-tiles.component.ts
+++ app/components/explore-tiles/explore-tiles.component.ts
@@ -8,26 +8,14 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
     styleUrls: ['./app/global/stylesheets/master.css'],
     directives: [ROUTER_DIRECTIVES, ExploreButtonComponent],
     providers: [],
-    inputs: ['nearByCities'],
+    inputs: ['nearByCities', 'citiesDisplay'],
 })
 
 export class ExploreTilesComponent implements OnInit {
 
-    nearByCities: Object;
     citiesDisplay: Array<any> = [];
 
-    transformData() {
-        var data = this.nearByCities;
-
-        // Transform city names to lowercase and push to array for display
-        for (var index in data) {
-            if(data[index].hasOwnProperty("city")) {
-                this.citiesDisplay.push(data[index].city.toLowerCase());
-            }
-        }
-    }
-
     ngOnInit() {
-        this.transformData();
+        console.log('hi', this.citiesDisplay);
     }
 }
