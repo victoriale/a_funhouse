4d508a6ab17410298f5eb3b8102096a94c10103f
Jonathan Van Winkle <jonathan.vanwinkle@sntmedia.com>
2016-03-17 15:19:48 -0500

    cleaned up interfaces to match the API data changes

--- app/global/global-interface.ts
+++ app/global/global-interface.ts
@@ -13,16 +13,22 @@ export interface MagData{
 export interface MagOverview {
     pageTitle?: string;
     menuTitle?: string;
-    address?: string;
-    city?: string;
-    state?: string;
-    zipCode?: string;
-    county?: string;
-    country?: string;
+    address?: address;
     price?: number;
     key?: string;
     content?: string[];
     photos?: string[];
+}
+
+export interface MagRecommendations {
+    pageTitle?: string;
+    menuTitle?: string;
+    similar?: Array<{
+        address?: address;
+        key?: string;
+        content?: string[];
+        photos?: string[];
+    }>;
 }
 
 export interface MagNeighborhood {
@@ -32,70 +38,23 @@ export interface MagNeighborhood {
     neighbors?: MagMap[];
 }
 
-export interface MagMap {
-    key?: string;
-    photo?: string;
-    listing_key: string;
-    listhub_key: string;
-    street_address: string;
-    city: string;
-    state: string;
-    zipcode: string;
-    list_price?: number;
-    lat: number;
-    lng: number;
-    geocoded: {
-        normalized_address: string;
-        geometry: {
-            bounds?: {
-                northeast?: {
-                    lat: number;
-                    lng: number;
-                }
-                southwest?: {
-                    lat: number;
-                    lng: number;
-                }
-            }
-            location_type: string;
-            viewport: {
-                northeast: {
-                    lat: number;
-                    lng: number;
-                }
-                southwest: {
-                    lat: number;
-                    lng: number;
-                }
-            }
-        }
-    }
-}
-
-export interface MagRecommendations {
+export interface MagContact {
     pageTitle?: string;
     menuTitle?: string;
-    similar?: Array<{
-        address?: string;
-        key?: string;
-        content?: string[];
-        photos?: string[];
-    }>;
+    address?: address;
+    price?: number;
+    key?: string;
+    content?: string[];
+    photos?: string[];
 }
 
 export interface MagAmenities {
     pageTitle?: string;
     menuTitle?: string;
-    address?: string;
+    address?: address;
     restaurant?: MagAmenity;
     grocery?: MagAmenity;
     bank?: MagAmenity;
-}
-
-export interface MagAmenity {
-    title?: string;
-    content?: string[];
-    photo?: string;
 }
 
 export interface MagSchools {
@@ -114,17 +73,55 @@ export interface MagDemographics {
     photo?: string;
 }
 
-export interface MagContact {
-    pageTitle?: string;
-    menuTitle?: string;
-    address?: string;
+export interface address {
+    fullStreetAddress?: string;
     city?: string;
-    state?: string;
-    zipCode?: string;
     county?: string;
+    state?: string;
+    postalCode?: string;
     country?: string;
-    price?: number;
-    key?: string;
-    content?: string[];
-    photos?: string[];
+    lat?: number;
+    lng?: number;
 }
+
+export interface MagMap {
+    key?: string;
+    photo?: string;
+    listing_key?: string;
+    listhub_key?: string;
+    address?: address;
+    list_price?: number;
+    geocoded?: {
+        normalized_address?: string;
+        geometry?: {
+            bounds?: {
+                northeast?: {
+                    lat?: number;
+                    lng?: number;
+                }
+                southwest?: {
+                    lat?: number;
+                    lng?: number;
+                }
+            }
+            location_type?: string;
+            viewport?: {
+                northeast?: {
+                    lat?: number;
+                    lng?: number;
+                }
+                southwest?: {
+                    lat?: number;
+                    lng?: number;
+                }
+            }
+        }
+    }
+}
+
+export interface MagAmenity {
+    title?: string;
+    content?: string[];
+    photo?: string;
+}
+
