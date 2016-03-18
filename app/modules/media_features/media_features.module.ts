b126944944b9cf01c3bf838519bac02ac891da8f
LarryHPham <larry@passfail.com>
2016-03-17 22:03:37 -0500

    started to plug in data for property image

--- app/modules/media_features/media_features.module.ts
+++ app/modules/media_features/media_features.module.ts
@@ -7,6 +7,7 @@ import {moduleHeader} from "../../components/module-header/module-header";
 import {MediaImages} from "../../components/media-images/media-images.component";
 import {PropertyListingInterface} from '../../global/global-interface';
 import {GlobalFunctions} from '../../global/global-functions';
+import {ListingProfileService} from '../../global/listing-profile.service';
 
 @Component({
     selector: 'media-features-module',
@@ -21,20 +22,63 @@ export class MediaFeatureModule implements OnInit{
     public trending: boolean;
     public prop_features: any;
     public profileType: string;
+
+    private propertyData: any;//data to send from module into components
+
     image_url = './app/public/placeholder_XL.png';
     featureHeading = "Features Of This Property";
     lastUpdate = "Last Updated: Thursday, March 03, 2016";
 
     @Input() propertyListingData: PropertyListingInterface;
 
-    constructor(private router: Router, private _params: RouteParams, private globalFunctions: GlobalFunctions){
+    constructor(private router: Router, private _params: RouteParams, private globalFunctions: GlobalFunctions, private _listingService: ListingProfileService){
         //Determine what page the profile header module is on
         this.profileType = this.router.hostComponent.name;
     }
 
     getData(){
-      var data = this.propertyListingData;
-      console.log('media feature data: ', data);
+      // console.log(this._params.get('address'));
+      this.propertyData = this._listingService.getPropertyListing(this._params.get('address'))
+      .subscribe(
+        data => {
+          this.propertyData = this.dataFormatter(data);
+        }
+      )
+    }
+
+    dataFormatter(data){
+      // console.log(data);
+      var convertedData = data;
+      var featureArray = [];
+      var imagesArray = [];
+
+      var listingHAVE = [];
+      var listingNOHAVE = [];
+      //set imagesArray to be returned into module's components
+      imagesArray = data.listingImages;
+
+      //run though a for loop to combine all the
+      for(var feature in data){
+        if(data[feature] === null){
+          //Incase we want to use or check why this listing does not have these features
+          listingNOHAVE.push(feature);
+        }else{
+          if(feature == 'listingImages')
+          listingHAVE.push(feature);
+          featureArray.push({featureName: feature,value:data[feature]});
+        }
+      }
+
+      // console.log('This House has these features', listingHAVE);
+      // console.log('This House does not have these features', listingNOHAVE);
+
+      // console.log(featureArray);
+      // console.log(imagesArray)
+
+      return {
+        features: featureArray,
+        images: imagesArray
+      };
     }
 
     //Build Module Title
@@ -62,11 +106,6 @@ export class MediaFeatureModule implements OnInit{
       this.setModuleTitle();
         this.trending = false;
         this.getData();
-        this.prop_features = [
-          {
-            details: "Price: $749,000"
-          }
-        ];
     }
 
     //On Change Call
