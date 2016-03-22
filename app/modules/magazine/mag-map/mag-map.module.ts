66c4d183b6f197518ecdecd6567576be42ca5b26
lynchtroop5 <christopher.lynch@sntmedia.com>
2016-03-21 20:21:08 -0500

    Brought live data into the contact, recommendations, and neighborhood modules. Also wired up the google map with live data and images.

--- app/modules/magazine/mag-map/mag-map.module.ts
+++ app/modules/magazine/mag-map/mag-map.module.ts
@@ -1,10 +1,13 @@
-import {Component, provide, OnInit,ViewEncapsulation, ViewChild, ElementRef} from 'angular2/core';
+import {Component, provide, OnInit,ViewEncapsulation, ViewChild, ElementRef, Injector} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {MagazineMap} from "../../../global/global-mag-service";
 import {MapMarkerComponent} from "../../../components/mapMarker/mapMarker.component";
+import {MagazinePage} from "../../../app-webpage/magazine.webpage";
+import {MagazineDataService} from "../../../global/global-mag-service";
+import {MagNeighborhood} from "../../../global/global-interface";
 
-declare var google: any;
-declare var jQuery: any;
+declare var google:any;
+declare var jQuery:any;
 
 @Component({
     encapsulation: ViewEncapsulation.None,
@@ -17,59 +20,73 @@ declare var jQuery: any;
 
 
 export class MagMapModule implements OnInit {
-    data: any;
+    imgURL:string;
+    imgAddress:string;
+    address:string;
+    data:MagNeighborhood;
 
-    constructor(private _magazineMapService:MagazineMap) {
+    constructor(private _injector:Injector, private _magazineDataService:MagazineDataService) {
+        // Scroll page to top to fix routerLink bug
+        window.scrollTo(0, 0);
+        this.address = _injector.get(MagazinePage).address;
     }
 
     getMagazineMap() {
-        this._magazineMapService.getMagazineMap().then(data => {
-            this.data = data;
-        });
+        this._magazineDataService.getMagazineData(this.address)
+            .subscribe(
+                magData => {
+                    this.data = magData.neighborhood.neighbors;
+                    var myLatlng = new google.maps.LatLng(parseFloat(this.data[0].address.lat), parseFloat(this.data[0].address.lng));
+                    var mapOptions = {
+                        zoom: 12,
+                        center: myLatlng
+                    };
+                    $('.mag_n1_img').css("background-image", 'url(' + this.data[0].photos[0] + ')');
+                    this.imgAddress = this.data[0].address.fullStreetAddress;
+                    this.imgURL = 'magazine/' + this.data[0].key + '/overview';
+                    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+                    for (var i = 0; i < this.data.length; i++) {
+                        var home = new google.maps.LatLng(parseFloat(this.data[i].address.lat), parseFloat(this.data[i].address.lng));
+                        if (+this.data[i].price >= 1000000) {
+                            var priceString = "$" + (Math.round(+this.data[i].price / 100000) / 10) + "M";
+                        }
+                        else if (+this.data[i].price <= 999) {
+                            var priceString = "$" + +this.data[i].price;
+                        } else {
+                            var priceString = "$" + (Math.round(+this.data[i].price / 1000)) + "K";
+                        }
+                        if (i == 0) {
+                            var winString = '<div class="googleMap_item focus" id="' + i + '">' + priceString + '</div>';
+                        } else {
+                            var winString = '<div class="googleMap_item" id="' + i + '">' + priceString + '</div>';
+                        }
+                        var infoWindow = new google.maps.InfoWindow({
+                            content: winString,
+                            position: home
+                        });
+                        infoWindow.open(map);
+                        google.maps.event.addListener(infoWindow, 'domready', function () {
+                            var infoWindow = jQuery('.gm-style-iw');
+                            infoWindow.prev().addClass('hide');
+                            infoWindow.next().addClass('hide');
+                            infoWindow.parent().addClass('zSize');
+                            jQuery('.googleMap_item').click(function () {
+                                var clicked = event.target;
+                                var index = this.id;
+                                jQuery('.mag_n1_img').css("background-image", 'url(' + magData.neighborhood.neighbors[index].photos[0] + ')');
+                                jQuery('.mag_n1_img_text').html(magData.neighborhood.neighbors[index].address.fullStreetAddress);
+                                $('.mag_n1_img_view').attr("href", 'magazine/' + magData.neighborhood.neighbors[index].key + '/overview');
+                                //e.preventDefault();
+                                $('.googleMap_item').removeClass('focus');
+                                $(this).addClass('focus');
+                            });
+                        });
+                    }
+                },
+                err => console.log("error in getData", err)
+            );
     }
-
     ngOnInit() {
-        this._magazineMapService.getMagazineMap().then(data => {
-            this.data = data;
-            //console.log(this.data);
-            var myLatlng = new google.maps.LatLng(this.data[1]);
-            var mapOptions = {
-                zoom: 12,
-                center: myLatlng
-            };
-            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-            for (var i = 0; i < data.length; i++) {
-                var home = new google.maps.LatLng(this.data[i]);
-
-                if (+this.data[i].list_price >= 1000000) {
-                    var priceString = "$" + (Math.round(+this.data[i].list_price / 100000) / 10) + "M";
-                } else {
-                    var priceString = "$" + (Math.round(+this.data[i].list_price / 1000)) + "K";
-                }
-                if (i == 0) {
-                    var winString = '<div class="googleMap_item focus" id="' + i + '">' + priceString + '</div>';
-                } else {
-                    var winString = '<div class="googleMap_item" id="' + i + '">' + priceString + '</div>';
-                }
-                var infoWindow = new google.maps.InfoWindow({
-                    content: winString,
-                    position: home
-                });
-                //console.log(this.data[i].photo);
-                infoWindow.open(map);
-                google.maps.event.addListener(infoWindow, 'domready', function () {
-                    var infoWindow = jQuery('.gm-style-iw');
-                    infoWindow.prev().addClass('hide');
-                    infoWindow.next().addClass('hide');
-                    infoWindow.parent().addClass('zSize');
-                    jQuery('.googleMap_item').click(function() {
-                        var index = document.getElementById("id").id;
-                        //console.log(index);
-                        jQuery('.mag_n1_img').css("background-image", 'url('+this.data[index].photo+')');
-                    });
-                });
-            }
-        });
+        this.getMagazineMap();
     }
-
 }
