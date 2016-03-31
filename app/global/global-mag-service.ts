c0e307a08fa8388bb20b4d352b2eb08a571c941a
Larry Pham <LarryHPham@users.noreply.github.com>
2016-03-31 06:16:41 -0500

    Update global-mag-service.ts

--- app/global/global-mag-service.ts
+++ app/global/global-mag-service.ts
@@ -15,30 +15,30 @@ import {WebApp} from "../app-layout/app.layout";
 @Injectable()
 
 export class MagazineDataService {
-    // cachedData : MagData;
-    // public partnerID: string;
-    // constructor( private _injector: Injector, public http: Http) {
-    //     let partnerParam = this._injector.get(WebApp);
-    //     this.partnerID = partnerParam.partnerID;
-    // };
-    //
-    // getMagazineData(address) {
-    //     if ( this.cachedData ) {
-    //         return Observable.of(this.cachedData);
-    //     } else {
-    //         if (this.partnerID == null) {
-    //             return this.http.get('http://dev-realestate-ai.synapsys.us/' + address)
-    //                 .map(res => res.json())
-    //                 .do((data) => {
-    //                     this.cachedData = data;
-    //                 });
-    //         } else {
-    //             return this.http.get('http://dev-realestate-ai.synapsys.us/' + address + '?partner=' + this.partnerID)
-    //                 .map(res => res.json())
-    //                 .do((data) => {
-    //                     this.cachedData = data;
-    //                 });
-    //         }
-    //     }
-    // }
+    cachedData : MagData;
+    public partnerID: string;
+    constructor( private _injector: Injector, public http: Http) {
+        let partnerParam = this._injector.get(WebApp);
+        this.partnerID = partnerParam.partnerID;
+    };
+    
+    getMagazineData(address) {
+        if ( this.cachedData ) {
+            return Observable.of(this.cachedData);
+        } else {
+            if (this.partnerID == null) {
+                return this.http.get('http://dev-realestate-ai.synapsys.us/' + address)
+                    .map(res => res.json())
+                    .do((data) => {
+                        this.cachedData = data;
+                    });
+            } else {
+                return this.http.get('http://dev-realestate-ai.synapsys.us/' + address + '?partner=' + this.partnerID)
+                    .map(res => res.json())
+                    .do((data) => {
+                        this.cachedData = data;
+                    });
+            }
+        }
+    }
 }
