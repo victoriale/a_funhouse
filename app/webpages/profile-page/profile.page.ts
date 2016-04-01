4ec9648a75fef39a2837399b90efeacffbaf605d
victoriale <le.victoriavan@gmail.com>
2016-04-01 11:46:20 -0500

    fix getting correct name for headline

--- app/webpages/profile-page/profile.page.ts
+++ app/webpages/profile-page/profile.page.ts
@@ -80,6 +80,10 @@ export class ProfilePage implements OnInit{
                     this.partnerCheck = true;
                     this.pageName = "My HouseKit";
                 }
+                this.headlineInteract = {
+                    title: 'Interact with ' + this.pageName,
+                    icon: 'fa-comment-o'
+                };
           })
         this.paramAddress = _params.get('address');
         window.scrollTo(0, 0);
@@ -197,10 +201,6 @@ export class ProfilePage implements OnInit{
         this.headlineOtherHomes = {
             title: 'Other Homes You May Be Interested In',
             icon: 'fa-heart-o'
-        };
-        this.headlineInteract = {
-            title: 'Interact with ' + this.pageName,
-            icon: 'fa-comment-o'
         };
     }
 }
