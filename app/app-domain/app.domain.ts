52104b721634cc9bec3ee999636a79561436154e
Jonathan Van Winkle <jonathan.vanwinkle@sntmedia.com>
2016-04-03 12:17:23 -0500

    Removed a typecast that didn't exist for WINDOW to fix Safari loading issue

--- app/app-domain/app.domain.ts
+++ app/app-domain/app.domain.ts
@@ -28,6 +28,7 @@ import {MyWebApp} from "../app-layout/app.mylayout";
 
 export class AppDomain {
     cityStateLocation: string = "WICHITA_KS";
-    constructor(private window: Window){
+    constructor(){
+        //console.log(window.location);
     }
 }
