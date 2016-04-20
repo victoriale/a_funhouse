ab8871f105cdaaae931d1a6f3f1fd1c45b954330
lutzklai <lutzklai@gmail.com>
2016-04-20 13:58:01 -0500

    remove consoles

--- app/global/search-service.ts
+++ app/global/search-service.ts
@@ -103,78 +103,78 @@ export class SearchService{
     }
 
     //API to transform data to link
-    dataToLink(data){
-        var count = 0;
-        var addressCount = 0;
-        var cityCount = 0;
-        var zipcodeCount = 0;
-        var maxCount = 5;
-        var searchArray = [];
-        var self = this;
-
-        //If location cities are not null in api return iterate through cities
-        if(typeof data.location_city !== 'undefined' && data.location_city !== null) {
-            data.location_city.forEach(function (item, index) {
-                if(cityCount < 5 && count < maxCount) {
-                    searchArray.push({
-                        title: self._globalFunctions.toTitleCase(item.city) + ', ' + item.state_or_province.toUpperCase(),
-                        page: 'Location-page',
-                        params: {
-                            loc: self._globalFunctions.toLowerKebab(item.city) + '-' + item.state_or_province.toLowerCase()
-                        }
-                    });
-                    //Increment count to ensure only 10 results are displayed and 5 city results are displayed
-                    cityCount++;
-                    count++;
-                }
-            });
-        }
-
-        //If addresses are not null in api return iterate through addresses
-        if(typeof data.address !== 'undefined' && data.address !== null) {
-            data.address.forEach(function (item, index) {
-                if(addressCount < 5 && count < maxCount){
-                    //Pass property_type and address_key so individual search components can build the magazine route (This is needed because the search components are on different levels of the router
-                    searchArray.push({
-                        property_type: item.property_type,
-                        title: self._globalFunctions.toTitleCase(item.full_street_address) + ', ' + self._globalFunctions.toTitleCase(item.city) + ', ' + item.state_or_province.toUpperCase(),
-                        //Default page and route params
-                        page: 'Profile-page',
-                        params: {
-                            address: item.address_key.toLowerCase()
-                        },
-                        address_key: item.address_key
-                    });
-                    //Increment counter to ensure only 10 results total are displayed and 5 address results are displayed
-                    addressCount++;
-                    count++;
-                }
-            });
-        }
-
-        //If zipcodes are not null in api return iterate through zipcodes
-        if(typeof data.zipcode !== 'undefined' && data.zipcode !== null && count < 10){
-            data.zipcode.forEach(function(item, index){
-                //If count is 10 skip over remaining zipcodes
-                if(zipcodeCount < 5 && count < maxCount){
-                    //Pass property_type and address_key so individual search components can build the magazine route (This is needed because the search components are on different levels of the router
-                    searchArray.push({
-                        property_type: item.property_type,
-                        title: item.zipcode + ' - ' + self._globalFunctions.toTitleCase(item.full_street_address) + ', ' + self._globalFunctions.toTitleCase(item.city) + ', ' + item.state_or_province.toUpperCase(),
-                        page: 'Profile-page',
-                        params: {
-                            address: item.address_key.toLowerCase()
-                        },
-                        address_key: item.address_key
-                    });
-                    //Increment count to ensure only 10 results are displayed and 5 zipcode results are displayed
-                    zipcodeCount++;
-                    count++;
-                }
-            });
-        }
-
-        return searchArray;
-    }
+    //dataToLink(data){
+    //    var count = 0;
+    //    var addressCount = 0;
+    //    var cityCount = 0;
+    //    var zipcodeCount = 0;
+    //    var maxCount = 5;
+    //    var searchArray = [];
+    //    var self = this;
+    //
+    //    //If location cities are not null in api return iterate through cities
+    //    if(typeof data.location_city !== 'undefined' && data.location_city !== null) {
+    //        data.location_city.forEach(function (item, index) {
+    //            if(cityCount < 5 && count < maxCount) {
+    //                searchArray.push({
+    //                    title: self._globalFunctions.toTitleCase(item.city) + ', ' + item.state_or_province.toUpperCase(),
+    //                    page: 'Location-page',
+    //                    params: {
+    //                        loc: self._globalFunctions.toLowerKebab(item.city) + '-' + item.state_or_province.toLowerCase()
+    //                    }
+    //                });
+    //                //Increment count to ensure only 10 results are displayed and 5 city results are displayed
+    //                cityCount++;
+    //                count++;
+    //            }
+    //        });
+    //    }
+    //
+    //    //If addresses are not null in api return iterate through addresses
+    //    if(typeof data.address !== 'undefined' && data.address !== null) {
+    //        data.address.forEach(function (item, index) {
+    //            if(addressCount < 5 && count < maxCount){
+    //                //Pass property_type and address_key so individual search components can build the magazine route (This is needed because the search components are on different levels of the router
+    //                searchArray.push({
+    //                    property_type: item.property_type,
+    //                    title: self._globalFunctions.toTitleCase(item.full_street_address) + ', ' + self._globalFunctions.toTitleCase(item.city) + ', ' + item.state_or_province.toUpperCase(),
+    //                    //Default page and route params
+    //                    page: 'Profile-page',
+    //                    params: {
+    //                        address: item.address_key.toLowerCase()
+    //                    },
+    //                    address_key: item.address_key
+    //                });
+    //                //Increment counter to ensure only 10 results total are displayed and 5 address results are displayed
+    //                addressCount++;
+    //                count++;
+    //            }
+    //        });
+    //    }
+    //
+    //    //If zipcodes are not null in api return iterate through zipcodes
+    //    if(typeof data.zipcode !== 'undefined' && data.zipcode !== null && count < 10){
+    //        data.zipcode.forEach(function(item, index){
+    //            //If count is 10 skip over remaining zipcodes
+    //            if(zipcodeCount < 5 && count < maxCount){
+    //                //Pass property_type and address_key so individual search components can build the magazine route (This is needed because the search components are on different levels of the router
+    //                searchArray.push({
+    //                    property_type: item.property_type,
+    //                    title: item.zipcode + ' - ' + self._globalFunctions.toTitleCase(item.full_street_address) + ', ' + self._globalFunctions.toTitleCase(item.city) + ', ' + item.state_or_province.toUpperCase(),
+    //                    page: 'Profile-page',
+    //                    params: {
+    //                        address: item.address_key.toLowerCase()
+    //                    },
+    //                    address_key: item.address_key
+    //                });
+    //                //Increment count to ensure only 10 results are displayed and 5 zipcode results are displayed
+    //                zipcodeCount++;
+    //                count++;
+    //            }
+    //        });
+    //    }
+    //
+    //    return searchArray;
+    //}
 
 }
