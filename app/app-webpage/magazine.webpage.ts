e100010b8c606a9ea8313fa48d12ba7571b9f0e4
lynchtroop5 <christopher.lynch@sntmedia.com>
2016-04-21 17:33:51 -0500

    Fixed the issue with multiple ads showing if the user quickly scrolls left or right throught the magazine.

--- app/app-webpage/magazine.webpage.ts
+++ app/app-webpage/magazine.webpage.ts
@@ -21,7 +21,7 @@ declare var jQuery:any;
 @Component({
     selector: 'magazine-page',
     templateUrl: './app/app-webpage/magazine.webpage.html',
-    
+
     directives: [Contact, Neighborhood, Recommendations, Amenities, MagHeaderModule, FooterComponent, ROUTER_DIRECTIVES, MagOverviewModule, NavLeftComponent, NavRightComponent, MagErrorModule],
     providers: [MagazineDataService],
 })
@@ -64,12 +64,13 @@ declare var jQuery:any;
     }
 ])
 
-export class MagazinePage {
+export class MagazinePage implements OnInit {
     address:string;
     toc:any;
     magazineData:MagData;
     currentIndex:any;
     showErrorPage:boolean = false;
+    timer:number;
 
     getMagServiceData() {
         this._magazineDataService.getMagazineData(this.address)
@@ -109,6 +110,52 @@ export class MagazinePage {
         return toc;
     }
 
+    displayAd() {
+        var embedURL = "http://content.synapsys.us/embeds/realestate/standard/joyful.js";
+        var widgetURL = "";  //ad-stack only (solo)
+        var domain = "joyfulhome.com";
+        if (jQuery('#adzone').width() == 300) {
+            var adUnitName = "joyfulhome_com_realestate_magazine_300x250";
+        } else if (jQuery('#adzone').width() == 468) {
+            var adUnitName = "joyfulhome_com_realestate_magazine_468x60";
+        } else if (jQuery('#adzone').width() == 320) {
+            var adUnitName = "joyfulhome_com_realestate_magazine_320x50";
+        } else {
+            // console.log('There be no size!');
+            return false;
+        }
+
+        var q = {
+            dom: domain,
+            type: "realestate_solo",
+            subd: false,
+            remn: true,
+            src: embedURL,
+            name: adUnitName,
+            widU: widgetURL,
+            widW: 0,  //the widget's width
+            widH: 0,  //the widget's height
+            adW: jQuery('#adzone').width(),  //the ad's width
+            adH: jQuery('#adzone').height(),  //the ad's height
+            ofx: 0,  //offset in the X direction that the ad-stack needs to be adjusted to match the designated ad-space for this widget
+            ofy: 0,  //offset in the Y direction that the ad-stack needs to be adjusted to match the designated ad-space for this widget
+            rand: Math.random() * 10000000000000000000,
+        };
+        var newScript = document.createElement("script");
+        newScript.src = "http://content.synapsys.us/l/n/index-mdb.php?" + Object.keys(q).map(function (key) {
+                return encodeURIComponent(key) + "=" + encodeURIComponent(q[key])
+            }).join("&");
+        jQuery('#adzone').html('');
+        jQuery('#adzone')[0].appendChild(newScript);
+    }
+
+    resetTimer() {
+        clearTimeout(this.timer);
+        this.timer = setTimeout(() => {
+            this.displayAd();
+        }, 500);
+    }
+
     buildNavigationElements() {
         this.currentIndex = jQuery("magtab-component>span>a.router-link-active").index();
         if (this.currentIndex < 1) {
@@ -127,6 +174,10 @@ export class MagazinePage {
         this.buildNavigationElements();
         //removes min-height that is used on joyfulHome page.
         jQuery('.page-wrapper').css('min-height', 0);
+    }
+
+    ngOnInit() {
+        this.resetTimer();
     }
 
 }
